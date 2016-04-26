250a9d07ce8df6890b7a3b9a3c257a08743f3606
guygoudeau <guy.goudeau@students.aie.edu.au>
2016-04-26 17:58:25 -0500

    Refactored code for more scalability
    Got line to draw, figured out how to get one diagonal direction, fixed
    bad code

--- Astars.py
+++ Astars.py
@@ -1,4 +1,3 @@
-import heapq
 import pygame as gfx
 
 black = (0, 0, 0)
@@ -38,13 +37,13 @@ class Node(object):
 class AStar(object):
 	def __init__(self, SearchSpace, Start, Goal):
 		self.open = []
-		heapq.heapify(self.open) #open list heapified to keep node with lowest F at the top
-		self.close = set() # closed list is set for fast lookup
-		self.nodes = [] # nodes list
+		self.close = [] # closed list is set for fast lookup
+		self.nodes = SearchSpace # nodes list
 		self.grid_height = 6
 		self.grid_width = 6
 		self.start = Start
 		self.goal = Goal
+		self.current = self.start
 		
 	def init_grid(self): # initialize list of nodes
 		walls = ((0, 5), (1, 0), (1, 1), (1, 5), (2, 3), (3, 1), (3, 2), (3, 5), (4, 1), (4, 4), (5, 1)) # unreachable nodes
@@ -52,31 +51,44 @@ class AStar(object):
 			for y in range(self.grid_height):
 				if (x, y) in walls:
 					reachable = False
+					n = Node(x,y, False)
 				else:
 					reachable = True
-				self.nodes.append(Node(x, y, reachable))
+					n = Node(x,y,True)
+				self.nodes.append(n)
 		self.start = self.get_node(0,0) # starting point
-		self.start.IsStart = True
-		self.goal = self.get_node(5, 5) # end goal
+		self.start.IsStart = True	
 		self.goal.IsGoal = True
-	
-	def get_heuristic(self, node): # Heuristic value H: distance between this node and goal multiplied by 10
+		
+	def manhatten(self, node): # Heuristic value H: distance between this node and goal multiplied by 10
 		return 10 * (abs(node.x - self.goal.x) + abs(node.y - self.goal.y)) # <- manhatten distance
+		
+	def diagonal(self, node):
+		xDistance = abs(node.x - self.goal.x)
+		yDistance = abs(node.x - self.goal.x)
+		if (xDistance > yDistance):
+			return 14*yDistance + 10*(xDistance-yDistance)
+		else:
+			return 14*xDistance + 10*(yDistance-xDistance)
 		
 	def get_node(self, x, y): # return a node based on x and y coords
 		return self.nodes[x * self.grid_height + y]
 		
 	def get_adjacent_nodes(self, node): # retrieve list of adjacent nodes to a specific node
 		nodes = []
-		if node.x < self.grid_width-1:
-			nodes.append(self.get_node(node.x+1, node.y))
-		if node.y > 0:
-			nodes.append(self.get_node(node.x, node.y-1))
-		if node.x > 0:
-			nodes.append(self.get_node(node.x-1, node.y))
-		if node.y < self.grid_height-1:
-			nodes.append(self.get_node(node.x, node.y+1))
-		return nodes
+		if node.reachable == True:
+			if node.x < self.grid_width-1:
+				nodes.append(self.get_node(node.x+1, node.y))
+			if node.y > 0:
+				nodes.append(self.get_node(node.x, node.y-1))
+			if node.x > 0:
+				nodes.append(self.get_node(node.x-1, node.y))
+			if node.y < self.grid_height-1:
+				nodes.append(self.get_node(node.x, node.y+1))
+				
+			if node.x < self.grid_width-1 and node.y > 0:
+				nodes.append(self.get_node(node.x-1,node.y+1))
+			return nodes
 		
 	def print_path(self): # print a path if found
 		node = self.goal
@@ -84,28 +96,57 @@ class AStar(object):
 			node = node.parent
 			print 'path: node: %d,%d' % (node.x, node.y)
 			
+	def draw_path(self, screen):
+		n = self.goal
+		while n.parent != None:
+			gfx.draw.line(screen, red, [n.left,n.top], [n.parent.left,n.parent.top], 5)
+			n = n.parent
+			
 	def update_node(self, adj, node): # Calculate G, H and F score and set parent node
-		adj.g = node.g + 10
-		adj.h = self.get_heuristic(adj)
-		adj.f = adj.h + adj.g
-		adj.parent = node
+		if adj.reachable == True:
+			adj.g = self.calc_g(node, adj)
+			adj.h = self.diagonal(adj)
+			adj.f = adj.h + adj.g
+			adj.parent = node
 		
-	def process(self): # implement the algorithm
-		heapq.heappush(self.open, (self.start.f, self.start)) # add starting node to open list heap
-		while len(self.open):
-			f, node = heapq.heappop(self.open) # pop node from heap
-			self.close.add(node) # add that node to closed list
-			if node is self.goal: # if the goal is found print the path and end
-				self.print_path()
-				break
-			adj_nodes = self.get_adjacent_nodes(node) # get the adjacent nodes for current node
+	def calc_g(self, node1, node2):
+		if (abs(self.nodes.index(node1) - self.nodes.index(node2)) == 6) or (abs(self.nodes.index(node1) - self.nodes.index(node2)) == 1):
+			return 10
+		if (abs(self.nodes.index(node1) - self.nodes.index(node2)) == 7) or (abs(self.nodes.index(node1) - self.nodes.index(node2)) == 5):
+			return 14
+			
+	def process(self, screen): # implement the algorithm		
+		self.current = self.start
+		self.open.append(self.current)
+		var = self.get_adjacent_nodes(self.current)
+		for i in var:
+			if i.reachable == True:
+				self.update_node(i, self.current)
+				self.open.append(i)
+			
+		self.open.remove(self.current)
+		self.close.append(self.current)
+		while len(self.open) != 0:
+
+			self.open.sort(key = lambda x: x.f) # sort by lowest f
+			self.current = self.open[0]
+			self.open.remove(self.current)
+			self.close.append(self.current)
+			
+			adj_nodes = self.get_adjacent_nodes(self.current) # get the adjacent nodes for current node
 			for adj_node in adj_nodes:
 				if adj_node.reachable and adj_node not in self.close: # if adj node is reachable and not in closed list
-					if (adj_node.f, adj_node) in self.open:
-						# if an adjacent node is in open list, check if current path is better
-						# than the last one found for this adjacent node
-						if adj_node.g > node.g + 10:
-							self.update_node(adj_node, node)
+					if adj_node not in self.open:
+						self.update_node(adj_node, self.current)
+						self.open.append(adj_node)
 					else:
-						self.update_node(adj_node, node)
-						heapq.heappush(self.open, (adj_node.f, adj_node)) # add adjacent node to open list
+						move = self.current.g + self.calc_g(self.current, adj_node)
+						if move < adj_node.g:
+							self.current.parent = adj_node.parent
+							adj_node.g = calc_g(self.current, adj_node)
+										
+			if self.goal in self.open: # if the goal is found in open list print & draw the path and end
+				self.print_path()
+				self.draw_path(screen)
+				return True
+		return False
--- TestAstars.py
+++ TestAstars.py
@@ -4,42 +4,44 @@ from Astars import *
 def main():
 
 	searchSpace = [] # create the search space to look through
-	walls = ((5, 0), (0, 1), (1, 1), (5, 1), (3, 2), (1, 3), (2, 3), (5, 3), (1, 4), (4, 4), (1, 5))
-	for x in range(6):
-		for y in range(6):
-			if (x, y) in walls:
-				reachable = False
-			else:
-				reachable = True
+	walls = ((5, 0), (0, 1), (1, 1), (5, 1), (3, 2), (1, 3), (2, 3), (5, 3), (1, 4), (4, 4), (1, 5)) # create walls
+	for x in range(6): # 6 rows
+		for y in range(6): # 6 columns
+			if (x, y) in walls: # if a node's x and y is in the walls list
+				reachable = False # it's unreachable
+			else: # else if a node's x and y is not in the walls lsit
+				reachable = True # it's reachable
 			n = Node(x, y, reachable)
 			searchSpace.append(n)
 	start = searchSpace[0] # starting point
 	start.IsStart = True
 	goal = searchSpace[35] # end goal
-	goal.IsGoal = True
-		
+	goal.IsGoal = True	
+	
 	pygame.init() # Initialize pygame
 	WINDOW_SIZE = [255, 255] # Set the HEIGHT and WIDTH of the screen
-	screen = pygame.display.set_mode(WINDOW_SIZE)
+	screen = pygame.display.set_mode(WINDOW_SIZE) # set screen equal to window size
 	pygame.display.set_caption("Astar") # Set title of screen
 	done = False # Loop until the user clicks the close button.
 	clock = pygame.time.Clock() # Used to manage how fast the screen updates
 	
-	As = AStar(searchSpace, start, goal)
-	As.init_grid()
+	screen.fill(black) # Set the screen background
+		
+	for i in searchSpace: # for every reachable node in the search space
+		i.draw(screen, white) # color them white
 
+	As = AStar(searchSpace, start, goal) # Create an AStar
+	#As.init_grid() # initialize the grid
+	As.process(screen) # Call the process for the algorithm
+	
+	#for n in searchSpace:
+		#print n.parent
+		
 	# -------- Main Program Loop -----------
 	while not done:
 		for event in pygame.event.get():  # User did something
 			if event.type == pygame.QUIT:  # If user clicked close
 				done = True	 # Flag that we are done so we exit this loop
-
-		screen.fill(black) # Set the screen background
-		
-		for i in searchSpace:
-			i.draw(screen, white)
-			
-		As.process()
 
 		clock.tick(60) # Limit to 60 frames per second
 
@@ -47,4 +49,4 @@ def main():
 
 	pygame.quit() # Be IDLE friendly. If you forget this line, the program will 'hang' on exit.
 
-main()
+main() # call main
